aac874cab83854c480acb707b48ed565e9185f22
Jennifer Sanders <jennys1000@gmail.com>
2020-11-03 22:12:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var greeting = "Hello, everybody!";
+var greeting = "";
 
-var specialGuest = "Neil deGrassse Tyson!"
+var specialGuest = "Neil deGrassse Tyson"
 
-var greetSpecialGuest = "Hello, " + specialGuest + "!";
+var greetSpecialGuest = "" + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let's talk about ${topic}.`
+var conversation = `${topic}.`
